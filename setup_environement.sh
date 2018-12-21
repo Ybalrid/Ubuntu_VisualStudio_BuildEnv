07e0e22adffcf74c5e7567d316600474dc5c6a2a
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-12-21 16:08:17 +0100

    Add base install script

---
+++ setup_environement.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "This is an environement installation script for Ubuntu made to automate the installation of a C++ developement environement"
+if [ "$(id -u)" != "0" ]; then
+	echo "Run me as root please :3"
+	exit 1
+fi
+
+
+#small post-install cleanup
+apt autoremove
+apt update
+apt upgrade
+apt install neofetch \
+	build-essential \
+	neovim \
+	cmake \
+	libsdl2-dev \
+	git \
+	git-lfs \
+	mercurial \
+	clang \
+	lldb \
+	valgrind \
+	cppcheck \
+	ninja-build \
+	clang-format \
+	libboost-all-dev \
+	autoconf \
+	openssh-server \
+	xxgdb \
+	ddd \
+	cgdb \
+	radare2 \
+	python-pip \
+	cowsay \
+	curl \
+	xterm \
+	software-properties-common \
+	apt-transport-https \
+	wget \
+
+#visual studio code, just in case you want to liveshare
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt update
+apt install code
+
+echo "starting/enabling ssh server for visual studio remote debuging"
+systemctl start ssh
+systemctl enable ssh
+
+echo "install gdbgui via pip"
+pip install gdbgui
+
+
+echo "done installing. - don't forget to configure git global name/mail"
+neofetch
+cowsay "Happy hacking!"
+
